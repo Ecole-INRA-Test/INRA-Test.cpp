c837fbb3e8b66f27e53a104d4f51b2e7dea9ef8d
jmgauthier <jm.gauthier25@gmail.com>
2013-12-03 10:42:34 +0100

    Ajout de tests dans LandSensorTest

--- test/LandSensorTest.cpp
+++ test/LandSensorTest.cpp
@@ -19,6 +19,7 @@ LandSensorTest::~LandSensorTest() {
 void LandSensorTest::setUp() {
   c1 = new Coordinates(0, 0);
   c2 = new Coordinates(0, 1);
+  c3 = new Coordinates(1, 0);
   l = new LandSensor();
 }
 
@@ -29,7 +30,17 @@ void LandSensorTest::getPointToPointEnergyCoefficientTest_1() {
   CPPUNIT_ASSERT(l->getPointToPointEnergyCoefficient(c1, c2) == 1.0);
 }
 
-void LandSensorTest::testFailedMethod() {
-    CPPUNIT_ASSERT(true);
+void LandSensorTest::getPointToPointEnergyCoefficientTest_2() {
+  double r1 = l->getPointToPointEnergyCoefficient(c1, c2);
+  CPPUNIT_ASSERT(l->getPointToPointEnergyCoefficient(c1, c3) == r1);
 }
 
+void LandSensorTest::getPointToPointEnergyCoefficientTest_3() {
+  double r1 = l->getPointToPointEnergyCoefficient(c1, c2);
+  CPPUNIT_ASSERT(l->getPointToPointEnergyCoefficient(c3, c2) == r1);
+}
+
+void LandSensorTest::getPointToPointEnergyCoefficientTest_4() {
+  double r1 = l->getPointToPointEnergyCoefficient(c1, c2);
+  CPPUNIT_ASSERT(l->getPointToPointEnergyCoefficient(c1, c2) == r1);
+}
