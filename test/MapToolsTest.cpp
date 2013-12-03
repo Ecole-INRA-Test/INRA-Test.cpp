f5983fffaeec1ade9f4cb19c04a3a8cd00046f3e
jmgauthier <jm.gauthier25@gmail.com>
2013-12-03 14:54:27 +0100

    Modification MapToolTest

--- test/MapToolsTest.cpp
+++ test/MapToolsTest.cpp
@@ -17,16 +17,37 @@ MapToolsTest::~MapToolsTest() {
 }
 
 void MapToolsTest::setUp() {
+  robotCoordinates = new Coordinates(5, 5);
 }
 
 void MapToolsTest::tearDown() {
 }
 
-void MapToolsTest::testMethod() {
+
+void MapToolsTest::nextForwardPositionTest_ALL() {
+  oracleCoordinates = new Coordinates(5, 6);
+  CPPUNIT_ASSERT(MapTools::nextForwardPosition(robotCoordinates, NORTH)->getX() == oracleCoordinates->getX() and MapTools::nextForwardPosition(robotCoordinates, NORTH)->getY() == oracleCoordinates->getY()); //Test du cas NORTH
+  oracleCoordinates = new Coordinates(5, 4);
+  CPPUNIT_ASSERT(MapTools::nextForwardPosition(robotCoordinates, SOUTH)->getX() == oracleCoordinates->getX() and MapTools::nextForwardPosition(robotCoordinates, SOUTH)->getY() == oracleCoordinates->getY()); //Test du cas SOUTH
+  oracleCoordinates = new Coordinates(6, 5);
+  CPPUNIT_ASSERT(MapTools::nextForwardPosition(robotCoordinates, EAST)->getX() == oracleCoordinates->getX() and MapTools::nextForwardPosition(robotCoordinates, EAST)->getY() == oracleCoordinates->getY()); //Test du cas EAST
+  oracleCoordinates = new Coordinates(4, 5);
+  CPPUNIT_ASSERT(MapTools::nextForwardPosition(robotCoordinates,WEST)->getX() == oracleCoordinates->getX() and MapTools::nextForwardPosition(robotCoordinates, WEST)->getY() == oracleCoordinates->getY()); //Test du cas WEST
+}
+
+void MapToolsTest::nextBackwardPositionTest_NORTH() {
     CPPUNIT_ASSERT(true);
 }
 
-void MapToolsTest::testFailedMethod() {
+void MapToolsTest::nextBackwardPositionTest_SOUTH() {
+    CPPUNIT_ASSERT(true);
+}
+
+void MapToolsTest::nextBackwardPositionTest_EAST() {
+    CPPUNIT_ASSERT(true);
+}
+
+void MapToolsTest::nextBackwardPositionTest_WEST() {
     CPPUNIT_ASSERT(true);
 }
 
--- test/MapToolsTest.h
+++ test/MapToolsTest.h
@@ -14,20 +14,31 @@
 class MapToolsTest : public CPPUNIT_NS::TestFixture {
     CPPUNIT_TEST_SUITE(MapToolsTest);
 
-    CPPUNIT_TEST(testMethod);
-    CPPUNIT_TEST(testFailedMethod);
+ 
+    CPPUNIT_TEST(nextForwardPositionTest_ALL);
+    CPPUNIT_TEST(nextBackwardPositionTest_NORTH);
+    CPPUNIT_TEST(nextBackwardPositionTest_SOUTH);
+    CPPUNIT_TEST(nextBackwardPositionTest_EAST);
+    CPPUNIT_TEST(nextBackwardPositionTest_WEST);
 
     CPPUNIT_TEST_SUITE_END();
 
 public:
+    Coordinates* oracleCoordinates;
+    Coordinates* robotCoordinates;
+
     MapToolsTest();
     virtual ~MapToolsTest();
     void setUp();
     void tearDown();
 
 private:
-    void testMethod();
-    void testFailedMethod();
+    void nextForwardPositionTest_ALL();
+
+    void nextBackwardPositionTest_NORTH();
+    void nextBackwardPositionTest_SOUTH();
+    void nextBackwardPositionTest_EAST();
+    void nextBackwardPositionTest_WEST();
 };
 
 #endif	/* MAPTOOLTESTS_H */
