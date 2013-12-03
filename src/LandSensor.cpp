1ccef2e5c8a18b7356293a73bc26973a8cc0d832
Cédric Joffroy <cjoffro2@femto-st.fr>
2013-12-03 10:47:15 +0100

    LandSensor conforme au Java

--- src/LandSensor.cpp
+++ src/LandSensor.cpp
@@ -15,14 +15,23 @@ LandSensor::~LandSensor() {
 
 double LandSensor::getPointToPointEnergyCoefficient(Coordinates* coordinate1, Coordinates* coordinate2) throw (int){
 	if(carte.find(coordinate1) == carte.end()){
-		carte[coordinate1] = Land::getLandByOrdinal(0);
+		try{
+		carte[coordinate1] = Land::getLandByOrdinal(rand()%5);
+		} catch(int e) {
+		throw ERROR_LANDSENSOR_FAIL;
+		}
 	}
 	if(carte.find(coordinate2) == carte.end()){
-		carte[coordinate2] = Land::getLandByOrdinal(0);
+		try{
+		carte[coordinate2] = Land::getLandByOrdinal(rand()%5);
+		} catch(int e) {
+		throw ERROR_LANDSENSOR_FAIL;
+		}
 	}
 	Land::Lands terrain1 = carte.find(coordinate1)->second;
 	Land::Lands terrain2 = carte.find(coordinate2)->second;
-
+	if (terrain1 == Land::INFRANCHISSABLE || terrain2 == Land::INFRANCHISSABLE)
+		throw ERROR_COORDINATES_INACCESSIBLE;
 	double result = (terrain1 + terrain2) / 2.0;
 	return result;
     }
